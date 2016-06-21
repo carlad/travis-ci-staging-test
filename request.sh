adecae7ff335aca52d6c387c4d31e44aa6b91ad4
carlad <carla@travis-ci.com>
2016-06-21 12:51:41 +0200

    add encrypted token

--- request.sh
+++ request.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-TOKEN=${TRAVIS_COM_KEY}
-curl -s -X POST \
+curl -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
-  -H "Authorization: token TOKEN" \
+  -H "Authorization: token ${TRAVIS_COM_KEY}" \
   -d "$body" \
   https://api.travis-ci.com/repo/travis-pro%2Ftest-project/requests
