7f531e7a7b94dce05c97246adb4242b0acb9db4f
carlad <carlad@users.noreply.github.com>
2016-06-21 12:14:05 +0200

    Create request.sh

---
+++ request.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token "${TRAVIS_COM_KEY}"" \
+  -d "$body" \
+  https://api.travis-ci.com/repo/travis-pro%2Ftest-repo-staging1/requests
