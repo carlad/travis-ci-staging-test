f3ecce8345d3a7103c30b2bdcf8a3ea716334075
carlad <carlad@users.noreply.github.com>
2016-06-21 12:34:49 +0200

    Update request.sh

--- request.sh
+++ request.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+TOKEN=${TRAVIS_COM_KEY}
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
-  -H "Authorization: token "${TRAVIS_COM_KEY}"" \
+  -H "Authorization: token TOKEN" \
   -d "$body" \
   https://api.travis-ci.com/repo/travis-pro%2Ftest-repo-staging1/requests
